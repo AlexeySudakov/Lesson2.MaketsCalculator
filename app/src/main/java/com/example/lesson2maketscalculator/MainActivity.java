db82e3bca95611818a59e8d5b08f8f46c7b4b58c
AlexeySudakov <sudakov-alexey@yandex.ru>
2022-03-29 13:56:14 +0300

    Calculator1 repo.

--- app/src/main/java/com/example/lesson2maketscalculator/MainActivity.java
+++ app/src/main/java/com/example/lesson2maketscalculator/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.lesson2maketscalculator;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
